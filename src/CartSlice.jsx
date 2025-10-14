76a7b44cacd7b72f2cf92b22e7d39eeb3625852d
Srilakshmi M Kashyap <kashyapsri1999@gmail.com>
2025-10-14 05:08:32 -0400

    Added cart items with redux cartslice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,22 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
+const initialState ={
+    items:[];
+};
+
+const cartSlice=createSlice({
+    name:"cart",
+    initialState,
+    reducers:{
+        addItem:(state,action)=>{
+            const newItem=action.payload;
+            const existingItem=state.items.find(
+                (item) =>item.name===newItem.name
+            );
+            if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ ...newItem, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      const nameToRemove = action.payload;
+      state.items = state.items.filter((item) => item.name !== nameToRemove);
     },
-    updateQuantity: (state, action) => {
 
-    
+     updateQuantity: (state, action) => {
+      const { name, amount } = action.payload;
+      const itemToUpdate = state.items.find((item) => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = amount;
+      }
     },
-  },
-});
+        }
+    }
+)
+
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
